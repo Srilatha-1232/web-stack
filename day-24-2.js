4f457b72ccd87646f3a4d9b2edcb9d04c9de2bb5
Srilatha-1232 <mullamurisrilatha43@gmail.com>
2026-02-13 23:06:40 +0530

    Add files via upload

---
+++ day-24-2.js
@@ -0,0 +1,37 @@
+// console.log("hello world!");
+
+// console.log(23);
+
+// var age = 23;
+// console.log(age);
+
+// var isYoung = true;
+// console.log(isyoung);
+
+// var age = 23;
+
+// age = 22;
+// console.log(" age : 123 ", age123);
+
+// var name = "rk";
+// console.log("name : rk");
+
+// arrays
+
+// var just = ["rk", "pk", "sk"];
+// console.log(just);
+// console.log(just[2]);
+
+// objects
+// var rk = {
+//   firstname: "ramakrishna",
+//   lastname: "prasad",
+//   age: 22,
+//   gneder: "male",
+//   crush: "nope",
+//   intrest: "mern",
+// };
+
+// console.log(rk);
+
+// console.log(rk.age);
