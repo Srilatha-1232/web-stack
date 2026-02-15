78d2e79309ea5c0c8c470b5edf1b764e1c80431b
Srilatha-1232 <mullamurisrilatha43@gmail.com>
2026-02-15 22:59:05 +0530

    Add files via upload

---
+++ day-26.js
@@ -0,0 +1,146 @@
+// console.log("ramu");
+
+//operators
+// +,-,/,*,%
+// var num1 = 21;
+// var num2 = 10;
+
+// // Addition
+// console.log("Addition: " + (num1 + num2));   // 31
+
+// // Subtraction
+// console.log("Subtraction: " + (num1 - num2)); // 11
+
+// // Multiplication
+// console.log("Multiplication: " + (num1 * num2)); // 210
+
+// // Division
+// console.log("Division: " + (num1 / num2)); // 2.1
+
+// // Modulus (Remainder)
+// console.log("Modulus: " + (num1 % num2)); // 1
+
+//  short hand operations
+//  a = a+b
+//  a+= b
+// var a = 5;
+// var b = 7;
+
+//   a = a+b; 5+7=12
+//  a=1 o/p : a+b = 1+7 =8 because recently assigned value will be printed
+// console.log(a + b);
+
+// increment and decrement
+
+// var num = 5;
+// console.log(num);
+// num++; // num = num+1;
+// console.log(num);
+
+// var num = 5;
+
+// // POST INCREMENT
+// console.log("Post Increment:");
+// console.log(num++);  // prints 5, then num becomes 6
+// console.log(num);    // prints 6
+
+// // PRE INCREMENT
+// num = 5;
+// console.log("\nPre Increment:");
+// console.log(++num);  // num becomes 6, then prints 6
+// console.log(num);    // prints 6
+
+// // POST DECREMENT
+// num = 5;
+// console.log("\nPost Decrement:");
+// console.log(num--);  // prints 5, then num becomes 4
+// console.log(num);    // prints 4
+
+// // PRE DECREMENT
+// num = 5;
+// console.log("\nPre Decrement:");
+// console.log(--num);  // num becomes 4, then prints 4
+// console.log(num);    // prints 4
+
+// comparision operator
+// num1 = 21;
+// num2 = 10;
+// console.log(num1 == num2);  //false
+
+// num1 = 21;
+// num2 = 10;
+// console.log(num1 != num2); //true
+
+// num1 = 21;
+// num2 = 10;
+// console.log(num1 > num2);   //true
+
+// num1 = 21;
+// num2 = 10;
+// console.log(num1 < num2);   //false
+
+// num1 = 21;
+// num2 = 10;
+// console.log(num1 >= num2);   //true
+
+// num1 = 21;
+// num2 = 10;
+// console.log(num1 <= num2);  //false
+
+// num1 = 2;
+// num2 = "2";
+// console.log(num1 == num2); //num == string == true
+// console.log(num1 === num2); //num === string == false
+//it strictly check the condition
+
+// var ramu = false;
+// // !false => true
+// //  !true => false
+// console.log(!ramu);
+
+// var age = 22;
+
+// if (age > 18) {
+//   console.log("he/she is eligible for vote!");
+// } else {
+//   console.log("not eligible for vote");
+// }
+
+//  ternary operator
+// var age = 10;
+// age > 18
+//   ? console.log("eligible for vote")
+//   : console.log("not eligible for vote");
+
+// keyword string name = value ;
+// condition > value
+// ? condition 1 : condition2;
+//   true // false  : true //false
+
+// var obj1 = {
+//   name: "rk",
+//   keymap: function () {
+//     console.log("hello world this is rk");
+//   },
+// };
+
+// obj1.keymap();
+
+// function sum(a, b) {
+//   console.log(a + b);
+// }
+
+// //calling function
+// sum(2, 3);
+
+// anonymous function
+// var sum = function (a, b) {
+//   console.log(a + b);
+// };
+// sum(5, 5);
+
+// var demo = function () {
+//   console.log("demo here");
+// };
+// demo();
+// sum(4, 5);
